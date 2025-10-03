3d1016927f7701396ec0589bfdc1b710b3db0e3a
Ros <gamingoj95@gmail.com>
2025-10-02 20:22:17 -0400

    Update consultation_queries.sql

--- customer_opinion_system/consultation_queries.sql
+++ customer_opinion_system/consultation_queries.sql
@@ -1,6 +1,14 @@
-SELECT COUNT(*) FROM clientes;
-SELECT COUNT(*) FROM productos;
-SELECT COUNT(*) FROM fuentes;
-SELECT COUNT(*) FROM encuestas;
-SELECT COUNT(*) FROM comentarios_sociales;
-SELECT COUNT(*) FROM resenas_web;
+USE CustomerOpinions;
+GO
+
+SELECT 'clientes' AS tabla, COUNT(*) AS total FROM clientes
+UNION ALL
+SELECT 'productos', COUNT(*) FROM productos
+UNION ALL
+SELECT 'fuentes', COUNT(*) FROM fuentes
+UNION ALL
+SELECT 'encuestas', COUNT(*) FROM encuestas
+UNION ALL
+SELECT 'comentarios_sociales', COUNT(*) FROM comentarios_sociales
+UNION ALL
+SELECT 'resenas_web', COUNT(*) FROM resenas_web;
